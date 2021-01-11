0028a2d858d1c9f61de80d90387cb5fb2d5ae295
guanhuaihai <huaihai.guan@zhenai.com>
2021-01-11 15:18:35 +0800

    clear test

--- tests/Unit/Database/ConnectionTest.php
+++ tests/Unit/Database/ConnectionTest.php
@@ -12,15 +12,8 @@ class ConnectionTest extends TestCase
 {
     public function testQuery()
     {
-        $config =  [
-            'host' => '10.51.8.39',
-            'port' => '8123',
-            'database' => 'za_dws',
-            'username' => 'finance_test',
-            'password' => 'zhenai.com',
-        ];
-        $connection = new Connection($config);
+        $connection = new Connection(['host' => 'localhost']);
 
-        var_dump($connection->query()->getConnection()->select("select ftime , contract_prefix, split_type, order_time, begin_time, split_month, order_id, memberid, cnt_month_days, product_id, sum(split_amt) split_amt from mytest where ftime >= '20210104' and ftime <= '20210110' group by ftime, contract_prefix, split_type, order_time, begin_time, split_month, order_id, memberid, cnt_month_days, product_id order by ftime desc limit 50 offset 0"));
+        $this->assertInstanceOf(Builder::class, $connection->query());
     }
 }
